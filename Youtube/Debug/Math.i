da30a759b0ff5ee79dee2cc98dc680b564b04fdd
lance-lh <imleehao@gmail.com>
2018-11-25 21:38:10 +0800

    compilation

---
+++ Youtube/Debug/Math.i
@@ -0,0 +1,9 @@
+#line 1 "d:\\c++ files\\youtube\\math.cpp"
+
+
+int Math(int a, int b)
+{
+	int ans = a * b;
+	return ans;
+	
+}
---
+++ Youtube/Math.cpp
@@ -0,0 +1,8 @@
+//#include <iostream>
+
+int Math(int a, int b)
+{
+	int ans = a * b;
+	return ans;
+	//#include "endofbrace.h"
+}
