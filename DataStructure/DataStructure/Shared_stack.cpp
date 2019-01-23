272a9bd283ebc172e35168c869dc5d62c9eb69e0
lance-lh <imleehao@gmail.com>
2019-01-23 16:21:23 +0800

    pop shared stack (not update poped array)

--- DataStructure/DataStructure/Shared_stack.cpp
+++ DataStructure/DataStructure/Shared_stack.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #define MAXSIZE 10
 
@@ -23,29 +24,38 @@ public:
 			return;  // zero, false
 		}
 
-		// wrong using switch
-		/*switch (stacknum)   
-		{
-			case 1:
-			{
-				data[++top1] == x;
-				break;
-			}
-
-			case 2:
-			{
-				data[--top2] = x;
-				break;
-			}
-
-		}*/
-
 		if (stacknum == 1)
 			data[++top1] = x;
 		if (stacknum == 2)
 			data[--top2] = x;
 	}
 
+	int Pop(int stacknum)
+	{
+		if (stacknum == 1)
+		{
+			if (top1 == -1)
+			{
+				std::cout << "Stack Underflow!" << std::endl;
+			}
+			else
+			{
+				return data[top1--];
+			}
+		}
+
+		if (stacknum == 2)
+		{
+			if (top2 == MAXSIZE)
+			{
+				std::cout << "Stack Underflow!" << std::endl;
+			}
+			else
+			{
+				return data[top2++];
+			}
+		}
+	}
 };
 
 int main()
@@ -63,5 +73,15 @@ int main()
 	}
 	std::cout << '\n';
 
+	s.Pop(1);
+	s.Pop(2);
+
+	for (int i : s.data)
+	{
+		std::cout << i << '\t';
+	}
+	std::cout << '\n';
+
 	std::cin.get();
+	return 0;
 }
